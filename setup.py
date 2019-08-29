f08164d1a4f3b63af00d12685e8d0722b2b8120c
algaebrown <b101102109@tmu.edu.tw>
2019-08-28 23:51:14 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='coinheritance',
+      version='0.1',
+      description='Pan-genome wide co-inheritance network to predict protein function',
+      url='http://github.com/algaebrowb/co-inheritance',
+      author='Hsuan-lin Her',
+      author_email='hsher@ucsd.edu',
+      license='MIT',
+      packages=['coinheritance'],
+      zip_safe=False)
